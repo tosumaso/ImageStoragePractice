911551de846a237f8e5fecce7bb51eb3dfa0160b
tosumaso <wakuwaku5712@gmail.com>
2022-02-21 20:25:33 +0900

    first commit

---
+++ src/main/java/ImageStorage/practice/ImageStoragePracticeApplication.java
@@ -0,0 +1,13 @@
+package ImageStorage.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageStoragePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageStoragePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ImageStorage/practice/ImageStoragePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ImageStorage.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageStoragePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
